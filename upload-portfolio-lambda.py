63877b02b48d64a3f252b4be13ff057904ee000c
Daniel Sablosky <DJSablosky@msn.com>
2017-09-20 09:04:07 -0400

    Update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -33,7 +33,11 @@ def lambda_handler(event, context):
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                mimetype = mimetypes.guess_type(nm)
+                if mimetype[0] == None:
+                    portfolio_bucket.upload_fileobj(obj, nm)
+                else:
+                    portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
                 portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
         print "Job done!"
